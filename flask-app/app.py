34434f9ce940c6f440605054198dfe3f069dcf45
ib-bib <ibrahimadilothman@gmail.com>
2025-04-19 06:05:03 +0400

    api update

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,39 +4,38 @@ import os
 from flask import Flask, jsonify
 from io import BytesIO
 from fuzzywuzzy import process
-# from supabase import create_client
+from supabase import create_client
 
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
 
-# SUPABASE_URL = os.getenv("SUPABASE_URL")
-# SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 
-# supabase = create_client(SUPABASE_URL, SUPABASE_API_KEY)
+bucket_name = "models"
 
-# bucket_name = "models"
-
-# def load_model_from_supabase(file_path: str):
-#     response = supabase.storage.from_(bucket_name).download(file_path)
-#     if response:
-#         print(f"Successfully fetched {file_path}")
-#         model = pickle.load(BytesIO(response))
-#         return model
-#     else:
-#         print(f"Failed to fetch {file_path}")
-#         return None
+def load_model_from_supabase(file_path: str):
+    response = supabase.storage.from_(bucket_name).download(file_path)
+    if response:
+        print(f"Successfully fetched {file_path}")
+        model = pickle.load(BytesIO(response))
+        return model
+    else:
+        print(f"Failed to fetch {file_path}")
+        return None
 
 
-# cf_model = load_model_from_supabase("cf_model.pkl")
-# cbf_model = load_model_from_supabase("cbf_model.pkl")
+cf_model = load_model_from_supabase("cf_model.pkl")
+cbf_model = load_model_from_supabase("cbf_model.pkl")
 
-cf_model = None
-cbf_model = None
+# cf_model = None
+# cbf_model = None
 
-with open("cf_model.pkl") as f:
-    cf = pickle.load(f)
+# with open("cf_model.pkl", "rb") as f:
+#     cf_model = pickle.load(f)
 
-with open("cbf_model.pkl") as f:
-    cbf = pickle.load(f)
+# with open("cbf_model.pkl", "rb") as f:
+#     cbf_model = pickle.load(f)
 
 
 if cf_model:
@@ -47,14 +46,15 @@ if cbf_model:
 
 app = Flask(__name__)
 
-cf_weight = 6.0
-cbf_weight = 12.0 - cf_weight
-
-latest_cf_recs = []
-latest_cbf_recs = []
-liked = []
-
 all_titles = list(cbf_model['title_idx'].keys())
+
+def balanced_weights(cf_raw: float):
+    cbf_raw = 12 - cf_raw
+    if cf_raw < cbf_raw:
+        return math.ceil(cf_raw), math.trunc(cbf_raw)
+    else:
+        return math.trunc(cf_raw), math.ceil(cbf_raw)
+    
 
 @app.route("/", methods=['GET'])
 def hello_world():
@@ -67,76 +67,57 @@ def search(movie) -> dict[str, int]:
     movie_id = int(cbf_model['title_id'][actual_title])
     return jsonify({"movie_title": actual_title, "movie_id": movie_id})
 
-@app.route("/cf/<movie>", methods=['GET'])
-def get_cf_recs(movie):
-    global latest_cf_recs
+@app.route("/cf/<movie_id>/<cf_weight>", methods=['GET'])
+def get_cf_recs(movie_id, cf_weight):
+    movie_id = int(movie_id)
+    cf_weight = int(cf_weight)
     recommendations = []
-    actual_movie = search(movie).get_json()
-    title = actual_movie['movie_title']
-    id = actual_movie['movie_id']
-    idx = cf_model['id_idx'][id]
+    idx = cf_model['id_idx'][movie_id]
     vec = cf_model['matrix'][idx]
     vec = vec.reshape(1,-1)
-    neighbors = cf_model['knn'].kneighbors(vec, return_distance=False, n_neighbors=math.ceil(cf_weight))
-    for i in range(1, math.ceil(cf_weight)):
+    neighbors = cf_model['knn'].kneighbors(vec, return_distance=False, n_neighbors=cf_weight)
+    for i in range(cf_weight):
         rec_idx = neighbors.item(i)
+        if idx == rec_idx:
+            continue
         rec_id = int(cf_model['idx_id'][rec_idx])
         rec_title = cbf_model['id_title'][rec_id]
-        recommendations.append({"rec_title": rec_title, "rec_id": rec_id})
-        latest_cf_recs.append(rec_id)
+        recommendations.append({"rec_title": rec_title, "rec_movie_id": rec_id})
 
-    return jsonify({"movie": title, "recommendations": recommendations, "model": "cf"})
+    return jsonify({"recommendations": recommendations, "model": "cf"})
 
-@app.route("/cbf/<movie>", methods=['GET'])
-def get_cbf_recs(movie) -> dict[str, list]:
-    global latest_cbf_recs
+@app.route("/cbf/<movie>/<cbf_weight>", methods=['GET'])
+def get_cbf_recs(movie_id, cbf_weight) -> dict[str, list]:
+    movie_id = int(movie_id)
+    cbf_weight = int(cbf_weight)
     recommendations = []
-    actual_movie = search(movie).get_json()
-    title = actual_movie['movie_title']
-    idx = cbf_model['title_idx'][title]
+    idx = cbf_model['id_idx'][movie_id]
     vec = cbf_model['matrix'][idx]
     vec = vec.reshape(1,-1)
-    neighbors = cbf_model['knn'].kneighbors(vec, return_distance=False, n_neighbors=math.ceil(cbf_weight))
-    for i in range(math.ceil(cbf_weight)):
+    neighbors = cbf_model['knn'].kneighbors(vec, return_distance=False, n_neighbors=cbf_weight)
+    for i in range(cbf_weight):
         rec_idx = neighbors.item(i)
         if idx == rec_idx:
             continue
         rec_title = cbf_model['idx_title'][rec_idx]
         rec_id = int(cbf_model['title_id'][rec_title])
-        recommendations.append({"rec_title": rec_title, "rec_id": rec_id})
-        latest_cbf_recs.append(rec_id)
+        recommendations.append({"rec_title": rec_title, "rec_movie_id": rec_id})
 
-    return jsonify({"movie": title, "recommendations": recommendations, "model": "cbf"})
+    return jsonify({"recommendations": recommendations, "model": "cbf"})
 
-@app.route("/recommend/<movie>", methods=['GET'])
-def get_hybrid_recs(movie):
-    cf_recs = get_cf_recs(movie).get_json()
-    cbf_recs = get_cbf_recs(movie).get_json()
+@app.route("/recommend/<movie_id>/<cf_weight>", methods=['GET'])
+def get_hybrid_recs(movie_id, cf_weight):
+    cf_weight = float(cf_weight)
+    cf_weight, cbf_weight = balanced_weights(float(cf_weight))
+    movie_id = int(movie_id)
+    cf_recs = get_cf_recs(movie_id, cf_weight).get_json()
+    cbf_recs = get_cbf_recs(movie_id, cbf_weight).get_json()
     return jsonify({
-        "movie": cf_recs['movie'], 
+        "movie_title": cbf_model['id_title'][movie_id],
         "cf": cf_recs['recommendations'], 
         "cbf": cbf_recs['recommendations']
         })
 
-@app.route("/like/<model>/<int:movie_id>", methods=['GET'])
-def like_model_recommendation(model, movie_id: int):
-    global cf_weight, cbf_weight
-    title = cbf_model['id_title'][movie_id]
-    if model == "cf":
-        if cf_weight < 10 and movie_id not in latest_cbf_recs:
-            cf_weight = cf_weight + 0.2
-    elif model == "cbf":
-        if cf_weight > 2 and movie_id not in latest_cf_recs:
-            cf_weight = cf_weight - 0.2
-    else:
-        return jsonify({"Error": "Tried to tamper with API request"})
-    cbf_weight = 12 - cf_weight
-    return jsonify({
-        "model": "Collaborative Filtering" if model == "cf" else "Content-based Filtering",
-        "movie": title,
-        "cf_weight": cf_weight,
-        "cbf_weight": cbf_weight
-        })
 
 if __name__ == "__main__":
     app.run(debug=True)
